429c0feb0be28252e088ea183d0e54c272c5f736
Christopher Wilson <ckwilson4@gmail.com>
2024-09-05 20:16:21 +0000

    Add cron job file

---
+++ src/cronjob/app.py
@@ -0,0 +1,49 @@
+import paramiko
+import paramiko.ecdsakey
+
+# Define the SSH key file path
+ssh_key_path = '/home/vscode/.ssh/id_ed25519.pem'
+
+# Define the SFTP server details
+hostname = '13.65.123.181'
+port = 22
+username = 'vscode'
+remote_path = '/home/vscode/uploadFiles'
+local_path = '/home/vscode/sftpFiles/hello.txt'
+
+# Create an SSH client
+ssh_client = paramiko.SSHClient()
+sftp_client = None
+
+try:
+    # Load the SSH key
+    private_key = paramiko.Ed25519Key(filename=ssh_key_path)
+
+    # Automatically add the server's host key
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    # Connect to the SFTP server
+    ssh_client.connect(hostname, port=port,
+                       username=username, pkey=private_key)
+
+    # Create an SFTP client
+    sftp_client = ssh_client.open_sftp()
+
+    try:
+        # Upload the file
+        sftp_client.put(local_path, remote_path)
+
+        print('File uploaded successfully!')
+    except Exception as e:
+        print(f'An error occurred: {e}')
+
+    finally:
+        # Close the SFTP client
+        sftp_client.close()
+
+except Exception as e:
+    print(f'An error occurred: {e}')
+
+finally:
+    # Close the SSH client
+    ssh_client.close()
