bffeb11c52f45aa7cf9789710e2b5a343bd735dd
Christopher Wilson <ckwilson4@gmail.com>
2024-09-09 09:07:49 +0000

    Update deployment

--- infra/tf/deploy.sh
+++ infra/tf/deploy.sh
@@ -62,7 +62,6 @@ prefix            = "rheem"
 location          = "southcentralus"
 subscription_id   = "494116cb-e794-4266-98e5-61c178d62cb4"
 vm_admin_username = "vscode"
-vm_admin_password = "P@ssw0rd123!"
 ssh_key      = "$sshKey"
 tags = {
   environment = "dev"
