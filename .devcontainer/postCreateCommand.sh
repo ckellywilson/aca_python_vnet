b864bb6fe81c64a9f78fad6a2f957a6dcf6786ee
Christoper Wilson <ckwilson4@gmail.com>
2024-09-16 05:03:25 +0000

    Add command to add pyenv

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Clone pyenv repository
+curl https://pyenv.run | bash
+
+# Set pyenv environment variables
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
